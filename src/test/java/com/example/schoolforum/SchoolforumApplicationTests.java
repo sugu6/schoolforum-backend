7ad904166fee2df9d5fd720c8c2a058ee24e678e
Sugeny <3342113181@qq.com>
2026-05-14 02:44:33 +0800

    test(schoolforum): 移除测试类中未使用的mock依赖
    删除了多余的Elasticsearch和搜索仓库相关的MockBean注入，清理测试代码冗余依赖

--- src/test/java/com/example/schoolforum/SchoolforumApplicationTests.java
+++ src/test/java/com/example/schoolforum/SchoolforumApplicationTests.java
@@ -1,28 +1,12 @@
 package com.example.schoolforum;
 
-import com.example.schoolforum.repository.PopularQueryRepository;
-import com.example.schoolforum.repository.PostSearchRepository;
-import com.example.schoolforum.repository.UserSearchRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class SchoolforumApplicationTests {
-
-    @MockBean
-    private ElasticsearchOperations elasticsearchOperations;
-
-    @MockBean
-    private PostSearchRepository postSearchRepository;
-
-    @MockBean
-    private UserSearchRepository userSearchRepository;
-
-    @MockBean
-    private PopularQueryRepository popularQueryRepository;
 
     @MockBean
     private JavaMailSender mailSender;
